6aaa8b6df8da5027312cb6bf84bbdd481d01c3d1
Permy Ghataorre <pghataorre@and.digital>
2023-07-19 11:05:46 +0100

    fixed lints

--- src/context/FixturesProvider/FixturesProvider.tsx
+++ src/context/FixturesProvider/FixturesProvider.tsx
@@ -2,14 +2,10 @@ import { useEffect, useState } from 'react';
 import { FixturesContext } from './FixturesContext';
 import getFixtures from '../contextApi/getFixtures';
 
-const FixturesProvider = ({children, tournamentId}: {children: JSX.Element, tournamentId: string}): JSX.Element  => {
+const FixturesProvider = ({children, tournamentId = '2'}: {children: JSX.Element, tournamentId: string}): JSX.Element  => {
   const [fixtures, setFixtures] = useState({});
   const [fixturesLoading, setFixturesLoading] = useState(false);
 
-  if (!tournamentId) {
-    tournamentId = '23';
-  }
-  
   useEffect(() => {
     (async () => {
       const fixtureData = await getFixtures(tournamentId);
@@ -18,7 +14,7 @@ const FixturesProvider = ({children, tournamentId}: {children: JSX.Element, tour
         setFixtures(fixtureData);
       }
     })();
-  }, []);
+  }, [tournamentId]);
 
   const fixturesContext = {
     fixtures,
