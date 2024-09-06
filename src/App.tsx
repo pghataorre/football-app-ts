a53c757db7af32b2748fb4c7261bb14dc0eec467
pghataorre <pghataorre@gmail.com>
2024-09-06 17:03:54 +0100

    chore: removed unwanted wrapper providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,36 @@ import { Routes, Route } from "react-router-dom";
 import Default from './pages/Default';
 import NoPage from './pages/NoPage';
 import Header from './components/Header/Header';
-import TeamProvider from './context/TeamProvider/TeamProvider';
-import TournamentsProvider from './context/TournamentProvider/TournamentsProvider';
-import FixturesProvider from './context/FixturesProvider/FixturesProvider';
+// import TeamProvider from './context/TeamProvider/TeamProvider';
+// import TournamentsProvider from './context/TournamentProvider/TournamentsProvider';
+// import FixturesProvider from './context/FixturesProvider/FixturesProvider';
 import ContentfulProvider from './context/ContentfulProvider/contentfulProvider';
-import { defaultTournament } from './constants/constants';
+//import { defaultTournament } from './constants/constants';
 import Teams from './pages/Teams';
 import AddPoints from './pages/AddPoints';
 import Managers from './pages/Managers';
 import AddManager from './pages/AddManager';
 import './styles/App.scss'
 import AddTournament from './pages/AddTournament';
+import ModalProvider from "./context/ModalProvider/ModalProvider";
 
 const App = (): JSX.Element => {
   return (
 		<ContentfulProvider>
-			<TeamProvider>
-				<TournamentsProvider>
-					<FixturesProvider tournamentId={defaultTournament}>
-						<div className='App'>
-							<Header />
-							<Routes>
-								<Route index element={<Default />} />
-								<Route path='*' element={<NoPage />} />
-								<Route path='/teams' element={<Teams />} />	
-								<Route path='/addpoints' element={<AddPoints />} />
-								<Route path='/managers' element={<Managers />} />		
-								<Route path="/addmanager" element={<AddManager />} />
-								<Route path="/addtournament" element={<AddTournament />} />
-							</Routes>
-						</div>
-					</FixturesProvider>
-				</TournamentsProvider>
-			</TeamProvider>
+			<ModalProvider>
+				<div className='App'>
+					<Header />
+					<Routes>
+						<Route index element={<Default />} />
+						<Route path='*' element={<NoPage />} />
+						<Route path='/teams' element={<Teams />} />	
+						<Route path='/addpoints' element={<AddPoints />} />
+						<Route path='/managers' element={<Managers />} />		
+						<Route path="/addmanager" element={<AddManager />} />
+						<Route path="/addtournament" element={<AddTournament />} />
+					</Routes>
+				</div>
+			</ModalProvider>
 		</ContentfulProvider>
   )
 }
